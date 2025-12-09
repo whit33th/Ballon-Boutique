425f98f41a50f1988ad0b3019497217d8ebcec36
Daniil Kaminskyi <deniil019@gmail.com>
2025-12-09 22:33:03 +0100

    update deps

--- components/Containers/Hero.tsx
+++ components/Containers/Hero.tsx
@@ -1,5 +1,5 @@
-import { getTranslations } from "next-intl/server";
 import Image from "next/image";
+import { getTranslations } from "next-intl/server";
 
 export const Hero = async () => {
   const t = await getTranslations("home");
