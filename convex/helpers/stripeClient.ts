1e900da951caae0cdc2a0669088ed3606464df48
Daniil Kaminskyi <deniil019@gmail.com>
2025-12-24 08:23:15 +0100

    stripeApi update

--- convex/helpers/stripeClient.ts
+++ convex/helpers/stripeClient.ts
@@ -3,7 +3,7 @@
 import Stripe from "stripe";
 
 export const STRIPE_API_VERSION =
-  "2025-11-17.clover" satisfies Stripe.LatestApiVersion;
+  "2025-12-15.clover" satisfies Stripe.LatestApiVersion;
 
 let cachedClient: Stripe | null = null;
 
